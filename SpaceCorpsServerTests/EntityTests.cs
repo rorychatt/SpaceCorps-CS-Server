7d2ab4ae11959221b604c9d3d3e1fa42b0f9b74d
rorychatt <pavel.rinne@gmail.com>
2024-07-15 14:03:41 +0200

    Feat: Player stats manipulation tests

--- SpaceCorpsServerTests/EntityTests.cs
+++ SpaceCorpsServerTests/EntityTests.cs
@@ -68,4 +68,102 @@ public class EntityTests
         Assert.DoesNotContain(item.ItemId, player.GetInventory().GetContents().Keys);
     }
 
+    [Fact]
+    public void TestPlayer_AddCredits()
+    {
+        var player = new Player();
+        player.GetStats().SetCredits(0);
+        var oldCredits = player.GetStats().GetCredits();
+        player.GetStats().AddCredits(100);
+        var newCredits = player.GetStats().GetCredits();
+        Assert.Equal(100, player.GetStats().GetCredits());
+        Assert.Equal(oldCredits + 100, newCredits);
+    }
+
+    [Fact]
+    public void TestPlayer_AddNegativeCredits()
+    {
+        var player = new Player();
+        player.GetStats().SetCredits(100);
+        var oldCredits = player.GetStats().GetCredits();
+        player.GetStats().AddCredits(-100);
+        var newCredits = player.GetStats().GetCredits();
+        Assert.Equal(0, player.GetStats().GetCredits());
+        Assert.Equal(oldCredits - 100, newCredits);
+    }
+
+    [Fact]
+    public void TestPlayer_AddThulium()
+    {
+        var player = new Player();
+        player.GetStats().SetThulium(0);
+        var oldThulium = player.GetStats().GetThulium();
+        player.GetStats().AddThulium(100);
+        var newThulium = player.GetStats().GetThulium();
+        Assert.Equal(100, player.GetStats().GetThulium());
+        Assert.Equal(oldThulium + 100, newThulium);
+    }
+
+    [Fact]
+    public void TestPlayer_AddNegativeThulium()
+    {
+        var player = new Player();
+        player.GetStats().SetThulium(100);
+        var oldThulium = player.GetStats().GetThulium();
+        player.GetStats().AddThulium(-100);
+        var newThulium = player.GetStats().GetThulium();
+        Assert.Equal(0, player.GetStats().GetThulium());
+        Assert.Equal(oldThulium - 100, newThulium);
+    }
+
+    [Fact]
+    public void TestPlayer_AddHonor()
+    {
+        var player = new Player();
+        player.GetStats().SetHonor(0);
+        var oldHonor = player.GetStats().GetHonor();
+        player.GetStats().AddHonor(100);
+        var newHonor = player.GetStats().GetHonor();
+        Assert.Equal(100, player.GetStats().GetHonor());
+        Assert.Equal(oldHonor + 100, newHonor);
+    }
+
+    [Fact]
+    public void TestPlayer_AddNegativeHonor()
+    {
+        var player = new Player();
+        player.GetStats().SetHonor(100);
+        var oldHonor = player.GetStats().GetHonor();
+        player.GetStats().AddHonor(-100);
+        var newHonor = player.GetStats().GetHonor();
+        Assert.Equal(0, player.GetStats().GetHonor());
+        Assert.Equal(oldHonor - 100, newHonor);
+    }
+
+    [Fact]
+    public void TestPlayer_AddExperience()
+    {
+        var player = new Player();
+        player.GetStats().SetExperience(0);
+        var oldExperience = player.GetStats().GetExperience();
+        player.GetStats().AddExperience(100);
+        var newExperience = player.GetStats().GetExperience();
+        Assert.Equal(100, player.GetStats().GetExperience());
+        Assert.Equal(oldExperience + 100, newExperience);
+    }
+
+    [Fact]
+    public void TestPlayer_AddNegativeExperience()
+    {
+        var player = new Player();
+        player.GetStats().SetExperience(100);
+        var oldExperience = player.GetStats().GetExperience();
+        player.GetStats().AddExperience(-100);
+        var newExperience = player.GetStats().GetExperience();
+        Assert.Equal(0, player.GetStats().GetExperience());
+        Assert.Equal(oldExperience - 100, newExperience);
+    }
+
+
+
 }
