340c65166c137bd52650081c85619b1e985a37ea
rorychatt <pavel.rinne@gmail.com>
2024-07-15 14:05:56 +0200

    Info: dependency cleanup

--- SpaceCorpsServerShared/Players/IStats.cs
+++ SpaceCorpsServerShared/Players/IStats.cs
@@ -1,9 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics.Contracts;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace SpaceCorpsServerShared.Players;
 public interface IStats
 {
--- SpaceCorpsServerShared/Players/Stats.cs
+++ SpaceCorpsServerShared/Players/Stats.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace SpaceCorpsServerShared.Players;
 
 public class Stats : IStats
