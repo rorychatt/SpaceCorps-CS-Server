4e201bb7fa572ad61205f810deac383d11d951ef
rorychatt <pavel.rinne@gmail.com>
2024-07-19 15:41:52 +0200

    Feat: test for actual stats reward

--- SpaceCorpsServerTests/RewardServerTests.cs
+++ SpaceCorpsServerTests/RewardServerTests.cs
@@ -50,5 +50,22 @@ public class RewardServerTests
         var rewardsFromServer = rewardsServer.GetRewardsForUser(playerId);
         Assert.Single(rewardsFromServer);
     }
-    
+
+    [Fact]
+    public async void Add_ThuliumReward_To_RewardServer_With_ValueCheck()
+    {
+        var playerId = Guid.NewGuid();
+        var stats = new Stats();
+        stats.SetThulium(10000);
+        var reward = new StatsReward(playerId, stats);
+        var rewardsServer = new RewardServer();
+        await rewardsServer.CreateRewardAsync(playerId, reward);
+
+        var rewardsFromServer = rewardsServer.GetRewardsForUser(playerId);
+        rewardsFromServer.TryDequeue(out var rewardable);
+        var statsReward = rewardable as StatsReward;
+
+        Assert.Equal(10000, statsReward!.Statistics.GetThulium());
+    }
+
 }
