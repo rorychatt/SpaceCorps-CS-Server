d9ccc7d16df02ee50afa86b2eeab796d1e90f9f3
rorychatt <pavel.rinne@gmail.com>
2024-07-19 12:53:56 +0200

    Feat: Fix create reward

--- SpaceCorpsServerShared/Statistics/IRewardServer.cs
+++ SpaceCorpsServerShared/Statistics/IRewardServer.cs
@@ -2,7 +2,7 @@ namespace SpaceCorpsServerShared.Statistics;
 
 public interface IRewardServer
 {
-    public Dictionary<Guid, List<IRewardable>> GetRewards()
+    public Dictionary<Guid, List<IRewardable>> GetRewards();
     void CreateReward(Guid playerId, IRewardable rewardable);
     IRewardable? HandleRewardsForUser(Guid playerId);
     List<IRewardable> GetRewardsForUser(Guid playerId);
