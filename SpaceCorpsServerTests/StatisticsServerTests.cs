4c7a0c5cf6a01261f9c8616ab33393689d4f8487
rorychatt <pavel.rinne@gmail.com>
2024-07-26 15:17:55 +0200

    Info: made port const

--- SpaceCorpsServerTests/StatisticsServerTests.cs
+++ SpaceCorpsServerTests/StatisticsServerTests.cs
@@ -9,7 +9,7 @@ public class StatisticsServerTests
     [Fact]
     public void TestStatisticsServer_Start_No_Crash()
     {
-        var port = 3000;
+        const int port = 3000;
         var loggerMock = new Mock<ILogger<Server>>();
         var server = new Server(loggerMock.Object, port);
 
